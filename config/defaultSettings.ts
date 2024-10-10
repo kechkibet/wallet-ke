01b646f05181c9db26ceef12b44c0b29acc72d0b
BLUECALF <leonardkiptala@gmail.com>
2024-10-10 10:22:29 +0300

    FIX: primary color and title

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -9,13 +9,13 @@ const Settings: ProLayoutProps & {
 } = {
   navTheme: 'light',
   // 拂晓蓝
-  colorPrimary: '#1890ff',
+  colorPrimary: '#FF8C00',
   layout: 'mix',
   contentWidth: 'Fluid',
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'Orange Wallet',
   pwa: true,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
