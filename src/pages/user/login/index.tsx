9de63364e5636500fec3aba01df4f52e8c2fcad4
BLUECALF <leonardkiptala@gmail.com>
2024-10-20 21:32:05 +0300

    fix: empty  phone number in send otp

--- src/pages/user/login/index.tsx
+++ src/pages/user/login/index.tsx
@@ -3,7 +3,7 @@ import { MobileOutlined, LockOutlined } from '@ant-design/icons';
 import { LoginForm, ProFormCaptcha, ProFormText } from '@ant-design/pro-components';
 import { Helmet, useIntl} from '@umijs/max';
 import { message, Tabs } from 'antd';
-import React, { useState } from 'react';
+import React, { useRef,useState } from 'react';
 import Settings from '../../../../config/defaultSettings';
 import PageTheme from '@/components/PageTheme';
 
@@ -12,7 +12,8 @@ const Login: React.FC = () => {
   const [type, setType] = useState<string>('mobile');
   const [correlationId, setCorrelationId] = useState<string | null>(null); // Correlation ID for OTP verification
   const intl = useIntl();
-
+  const formRef = useRef<any>(null); // Reference to the form
+  
   const handleSubmit = async (values: { mobile: string; otp: string }) => {
     try {
       // Verify OTP
@@ -38,6 +39,12 @@ const Login: React.FC = () => {
 
   const handleSendOtp = async (phone: string) => {
     try {
+      if(phone === '')
+      {
+        message.error('Phone is required to send');
+        return
+      }
+    
       const result = await sendOtp(phone);
 
       if (result?.correlationId) {
@@ -62,6 +69,7 @@ const Login: React.FC = () => {
           </title>
         </Helmet>
         <LoginForm
+          formRef={formRef}
           contentStyle={{
           minWidth: 280,
           maxWidth: '75vw',
@@ -103,7 +111,17 @@ const Login: React.FC = () => {
             rules={[
               { required: true, message: 'Please enter the OTP!' },
             ]}
-            onGetCaptcha={async (phone) => handleSendOtp(phone)}
+            onGetCaptcha={async () => {
+              // Extract the latest phone number from the form
+              const formValues = await formRef.current?.validateFields(['mobile']);
+              const phone = formValues?.mobile;
+
+              if (phone) {
+                await handleSendOtp(phone);
+              } else {
+                message.error('Please enter a valid phone number before sending OTP.');
+              }
+            }}
             captchaTextRender={(timing, count) =>
               timing ? `${count}` : 'Send OTP'
             }
